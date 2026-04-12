014b206d6b3544ffc68891dacea91535072009d1
Johan Malm <johanmalm@MacBook-Air-som-tillhor-Johan.local>
2026-04-12 21:17:33 +0200

    Fix: objektkort centrerad text, overlay-stil konsekvent på hem och till salu

--- web/app/themes/standardsite/resources/views/page-objekt.blade.php
+++ web/app/themes/standardsite/resources/views/page-objekt.blade.php
@@ -67,16 +67,25 @@ function fasad_unserialize_listing($raw) {
         $status = ($status_obj && !empty($status_obj->tag)) ? $status_obj->tag : '';
         $status_label = ($status_obj && !empty($status_obj->alias)) ? strtoupper($status_obj->alias) : strtoupper($status);
       @endphp
-      <a href="{{ home_url('/objekt/' . get_post_field('post_name', $pid)) }}" class="objekt-kort" data-status="{{ $status }}">
-        <div class="objekt-kort-bild" @if($img_url) style="background-image:url('{{ $img_url }}')" @endif>
-          @if($status)
-            <span class="objekt-status objekt-status--{{ $status }}">{{ $status_label }}</span>
+      <a href="{{ home_url('/objekt/' . get_post_field('post_name', $pid)) }}" class="objekt-kort-inner" data-status="{{ $status }}">
+        <div class="objekt-bild">
+          @if($img_url)
+            <img src="{{ $img_url }}" alt="{{ $address }}">
+          @else
+            <div class="objekt-bild-placeholder"></div>
           @endif
-        </div>
-        <div class="objekt-kort-info">
-          <div class="objekt-adress">{{ $address }}@if($city), {{ $city }}@endif</div>
-          @if($price)<div class="objekt-pris">{{ $price }}</div>@endif
-          @if($type)<div class="objekt-typ">{{ $type }}</div>@endif
+          @if($status)
+            <div class="objekt-status objekt-status--{{ $status }}">{{ $status_label }}</div>
+          @endif
+          <div class="objekt-overlay">
+            <div class="objekt-info">
+              <div class="objekt-adress">{{ $address }}@if($city), {{ $city }}@endif</div>
+              @if($price)<div class="objekt-pris">{{ $price }}</div>@endif
+              <div class="objekt-meta">
+                @if($type)<span>{{ $type }}</span>@endif
+              </div>
+            </div>
+          </div>
         </div>
       </a>
     @endwhile
