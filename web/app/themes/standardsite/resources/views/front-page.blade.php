ed7f4d884dd0ed44a460d9fe7c5bd425bfb3c0a1
Johan Malm <johanmalm@MacBook-Air-som-tillhor-Johan.local>
2026-04-17 09:23:18 +0200

    Oscars: navy-tema, mosaikgrid, objektsidor, mobilfixar, dropdown-meny, typsnitt

--- web/app/themes/standardsite/resources/views/front-page.blade.php
+++ web/app/themes/standardsite/resources/views/front-page.blade.php
@@ -10,47 +10,46 @@
       <h2 class="sektion-rubrik">{{ $fp_listings_rubrik ?? 'Aktuella objekt' }}</h2>
     </div>
     @if(!empty($listings))
-      <div class="objekt-grid">
-        @foreach($listings as $listing)
-          <div class="objekt-kort">
-            <a href="{{ home_url('/objekt/' . $listing->slug) }}" class="objekt-kort-inner">
-              <div class="objekt-bild">
-                @if($listing->image)
-                  <img src="{{ $listing->image }}" alt="{{ $listing->address }}">
-                @else
-                  <div class="objekt-bild-placeholder"></div>
-                @endif
-                @if($listing->status)
-                  <div class="objekt-status objekt-status--{{ $listing->status }}">
-                    {{ ucfirst($listing->status) }}
-                  </div>
-                @endif
-                <div class="objekt-overlay">
-                  <div class="objekt-info">
-                    @if($listing->address)
-                      <div class="objekt-adress">{{ $listing->address }}</div>
-                    @endif
-                    @if($listing->price)
-                      <div class="objekt-pris">{{ $listing->price }}</div>
-                    @endif
-                    <div class="objekt-meta">
-                      @if($listing->type) <span>{{ $listing->type }}</span> @endif
-                      @if($listing->rooms) <span>{{ $listing->rooms }}</span> @endif
-                      @if($listing->area) <span>{{ $listing->area }}</span> @endif
-                    </div>
-                  </div>
-                </div>
+      <div class="mosaik-grid">
+        @foreach($listings as $i => $listing)
+          <a href="{{ home_url('/objekt/' . $listing->slug) }}"
+             class="mosaik-kort {{ $i === 0 ? 'mosaik-kort--stor' : '' }}">
+            <div class="mosaik-bild" style="background-image:url('{{ $listing->image }}')">
+              @if(!$listing->image)
+                <div style="width:100%;height:100%;background:#243558;"></div>
+              @endif
+            </div>
+            @if($listing->status)
+              <div class="objekt-status objekt-status--{{ $listing->status }}">
+                @php echo match($listing->status) {
+                  'sald' => 'Såld', 'kommande' => 'Kommande',
+                  'tillsalu' => 'Till salu', 'budgivning' => 'Budgivning',
+                  default => ucfirst($listing->status)
+                }; @endphp
               </div>
-            </a>
-          </div>
+            @endif
+            <div class="mosaik-info">
+              @if($listing->address)
+                <div class="mosaik-adress">{{ $listing->address }}</div>
+              @endif
+              @if($listing->price)
+                <div class="mosaik-pris">{{ $listing->price }}</div>
+              @endif
+              <div class="mosaik-meta">
+                @if($listing->rooms) <span>{{ $listing->rooms }}</span> @endif
+                @if($listing->area) <span>{{ $listing->area }}</span> @endif
+              </div>
+            </div>
+          </a>
         @endforeach
+      </div>
+      <div style="text-align:center;margin-top:48px;">
+        <a href="{{ home_url('/objekt') }}" class="btn-primary">Se alla objekt</a>
       </div>
     @else
       <p class="objekt-tomma">Inga objekt tillgängliga just nu.</p>
     @endif
-    <div style="margin-top:40px;text-align:center;">
-      <a href="{{ home_url('/objekt') }}" class="btn-primary">Se alla objekt</a>
-    </div>
+
   </div>
 </section>
 
